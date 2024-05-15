93d13fdc15f3254a9839649da05df923e9ce0448
DELL <DELL@DESKTOP-B3T700B>
2024-05-15 17:29:46 -0500

    SUBIENDO LAS COSAS

--- src/Controlador/CCEmpresa.java
+++ src/Controlador/CCEmpresa.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JOptionPane;
 
 public class CCEmpresa {
     Connection con;
@@ -25,15 +26,15 @@ public class CCEmpresa {
         
         while(rs.next()) {
             CMEmpresa cli = new CMEmpresa();
-            cli.setIdEmpresa(rs.getInt("id_producto"));
-            cli.setIdEmpleado(rs.getInt("id_producto"));
-            cli.setIdProvedor(rs.getInt("id_producto"));
-            cli.setIdInventario(rs.getInt("id_producto"));
-            cli.setNomEmpresa(rs.getString("nom_producto"));            
-            cli.setDirEmpresa(rs.getString("nom_categprod"));
-            cli.setNumEmpleados(rs.getString("nom_categprod"));
-            cli.setUserEmpresa(rs.getString("nom_categprod"));
-            cli.setPassEmpresa(rs.getString("nom_categprod"));
+            cli.setIdEmpresa(rs.getInt("id_empresa"));
+            cli.setIdEmpleado(rs.getInt("id_empleado"));
+            cli.setIdProvedor(rs.getInt("id_provedor"));
+            cli.setIdInventario(rs.getInt("id_inventario"));
+            cli.setNomEmpresa(rs.getString("nom_empresa"));            
+            cli.setDirEmpresa(rs.getString("dir_empresa"));
+            cli.setNumEmpleados(rs.getString("num_empleados"));
+            //cli.setUserEmpresa(rs.getString(""));
+            //cli.setPassEmpresa(rs.getString("nom_categprod"));
             listacli.add(cli);
         }
     }
@@ -42,6 +43,65 @@ public class CCEmpresa {
     }
     return listacli;
 }
-
+    public int InsertarEmpresa (Object obj []){
+         int est = 0;
+         String ConSql = "INSERT INTO EMPRESA(nom_empresa,dir_empresa,num_empleados) VALUES(?,?,?)";
+         
+         try {
+             con = cn.conectar();
+             ps = con.prepareStatement(ConSql);
+             ps.setObject(1, obj[0]);
+             ps.setObject(2, obj[1]);
+             ps.setObject(3, obj[2]);
+             //si no te funciona = ps.setObject(4, obj[3]);
+             
+             est = ps.executeUpdate();
+             
+          
+         }
+         catch (Exception e){
+             System.out.println("Error al insertar"+e.getMessage());
+         }
+         
+         return est;
+    }
+    public int ActualizarEmpresa(Object obj[]){
+        int est=0;
+        String Consql="UPDATE EMPRESA SET nom_empresa=?, dir_empresa=?,num_empleados=?,id_empleado,id_provedor,id_inventario WHERE id_empresa=? ";
+        System.out.println(Consql);
+        try{
+            con=cn.conectar();
+            ps=con.prepareStatement(Consql);
+            ps.setObject(1, obj[0]); //nombre
+            ps.setObject(2, obj[1]); //costo
+            ps.setObject(3, obj[2]); //stock
+            ps.setObject(4, obj[3]); //categorias
+            ps.setObject(5, obj[4]); //id
+            ps.setObject(6, obj[5]); //id
+            ps.setObject(7, obj[6]); //id
+            ps.setObject(8, obj[7]); //id
+            est=ps.executeUpdate();
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Error al Actualizar al Producto");
+        }
+        return est;
+    }
+    public void BorrarProducto(int idcli)
+    {
+        String ConSql="DELETE FROM EMPRESA WHERE id_empresa=? ";
+        System.out.println(ConSql);
+        try 
+        {
+            con=cn.conectar(); //conectamos
+            ps=con.prepareStatement(ConSql);
+            ps.setInt(1, idcli);
+            ps.executeUpdate();
+        } 
+        catch (Exception e) 
+        {
+            JOptionPane.showMessageDialog(null, "Error al borrar Producto"+ e.getMessage());
+        }
+    }
+    
     
 }
